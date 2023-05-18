9b1fee14a58f080232ad92bdf00d68cb7013ee09
renatoatanes <RM552029@fiap.com.br>
2023-05-18 20:18:42 -0300

    arrumando html + js (exige correção)

--- cp3.js
+++ cp3.js
@@ -1,9 +1,46 @@
-function validarSenha() {
+var formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    
+    var primeiroNome = document.getElementById("primeiroNome").value;
+    var segundoNome = document.getElementById("segundoNome").value;
+    var email = document.getElementById("email").value;
     var senha = document.getElementById("senha").value;
-
-    if (senha.length >= 8) {
-        console.log("Senha válida!");
-    } else {
-        console.log("A senha deve ter pelo menos 8 caracteres.");
+    var confirmarSenha = document.getElementById("confirmarSenha").value;
+    
+    if (primeiroNome === "") {
+        alert("Por favor, informe o Primeiro Nome.");
+        return;
     }
-}
+    
+    if (segundoNome === "") {
+        alert("Por favor, informe o Segundo Nome.");
+        return;
+    }
+    
+    if (email === "") {
+        alert("Por favor, informe o Email.");
+        return;
+    }
+    
+    if (senha === "") {
+        alert("Por favor, informe a Senha.");
+        return;
+    }
+
+    if (senha.length < 8) {
+        alert("A Senha deve ter pelo menos 8 caracteres.");
+    }
+    
+    if (confirmarSenha === "") {
+        alert("Por favor, informe a Confirmação de Senha.");
+        return;
+    }
+    
+    if (senha !== confirmarSenha) {
+        alert("A Senha e a Confirmação de Senha não correspondem.");
+        return;
+    }
+    alert("Formulário válido. Dados enviados com sucesso!");
+});
+
