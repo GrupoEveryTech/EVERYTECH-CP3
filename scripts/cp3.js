65ffee1f09f0fd669884241051f1353cce98149a
leonardobarral <rm551472@fiap.com.br>
2023-05-22 00:01:23 -0300

    últimas correções

--- scripts/cp3.js
+++ scripts/cp3.js
@@ -57,4 +57,35 @@ formulario.addEventListener("submit", function(event) {
 
 });
 
+// validação dos usuario 
+
+let listaDeUsuarios = [
+
+    { 
+
+        nome: "renato",
+        segundonome: "atanes",
+        email:"rm552029",
+        senha:"123456",
+        confirmacaosenha:"123456"
+        
+
+     
+    }
+];
+
+localStorage.setItem("listaUser" ,JSON.stringify(listaDeUsuarios));
+
+const toggleDarkModeInput = document.getElementById('toggleDarkMode');
+const body = document.body;
+
+toggleDarkModeInput.addEventListener('change', function() {
+  if (toggleDarkModeInput.checked) {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+});
+
+
 
